60426bfb0c8cf45b5e5c0140a5214fd2220ebd18
Dhruvin Pambhar <dhruvinpambhar0609@gmail.com>
2024-01-16 22:36:23 +0530

    batsmen + live score

--- frontend/src/components/Home/Admin_home.jsx
+++ frontend/src/components/Home/Admin_home.jsx
@@ -57,7 +57,6 @@ function Admin_home() {
             {tournaments.map((tournament) => (
                 <a
                     key={tournament._id}
-                    href="#"
                     className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
                     onClick={() => handleTournamentClick(tournament)}
                 >
